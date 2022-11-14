243e6b569f0e3d32052fac292acdf6a2788363cc
Garalulu <pso9809@gmail.com>
2022-11-14 23:20:47 +0900

    port issue

--- run.py
+++ run.py
@@ -20,14 +20,15 @@ class Worker(threading.Thread):
        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
            s.bind((HOST, PORT))
            s.listen()
-           conn, addr = s.accept()
-           with conn:
-               print(f"Connected by {addr}")
-               while True:
-                   data = conn.recv(1024)
-                   if not data:
-                       break
-                   conn.sendall(data)
+           while True:
+               conn, addr = s.accept()
+               with conn:
+                   print(f"Connected by {addr}")
+                   while True:
+                       data = conn.recv(1024)
+                       if not data:
+                           break
+                       conn.sendall(data)
 
 import nextcord
 intents = nextcord.Intents.default()
